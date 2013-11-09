95fcc6eebaba30bf2cafa9fd7dff67b1059af701
Noah Goldman <noahg34@gmail.com>
2013-11-09 10:11:25 +0000

    the api is working much better than before and tests are passing

--- daemon/rtorrent.py
+++ daemon/rtorrent.py
@@ -1,27 +1,48 @@
 import xmlrpclib
+from time import time
 
 class RTorrent:
-	def __init__(self):
-		self.proxy = xmlrpclib.ServerProxy("http://localhost:500/")
+    def __init__(self):
+        self.proxy = xmlrpclib.ServerProxy("http://localhost:500")
+        self.delay = 0
 
-	def add_torrent_file(self, filedata):
-		self.proxy.load_raw_start(filedata)
+    def reset_timer(self):
+        self.delay = time() + 5
 
-	def add_torrent_magnet(self, magnet):
-		self.proxy.load_start(magnet)
-		return infohash
+    def add_torrent_file(self, filedata):
+        self.proxy.load_raw_start(xmlrpc.Binary(filedata))
+        self.reset_timer()
 
-	def get_completed_bytes(self,infohash):
-		return self.proxy.d.get_completed_bytes(infohash)
+    def add_torrent_magnet(self, magnet):
+        self.proxy.load_start(magnet)
+        self.reset_timer()
 
-	def get_down_rate(self,infohash):
-		return self.proxy.d.get_down_rate(infohash)
+    def get_completed_bytes(self,infohash):
+        while time() < self.delay:
+            pass
+        return self.proxy.d.get_completed_bytes(infohash)
 
-	def get_size_bytes(self,infohash):
-		return self.proxy.d.size_bytes(infohash)
+    def get_down_rate(self,infohash):
+        while time() < self.delay:
+            pass
+        return self.proxy.d.get_down_rate(infohash)
 
-	def close(self,infohash):
-		self.proxy.d.close(infohash)
+    def get_size_bytes(self,infohash):
+        while time() < self.delay:
+            pass
+        return self.proxy.d.size_bytes(infohash)
 
-	def get_active_infohashes(self):
-		self.proxy.download_list()
+    def close(self,infohash):
+        while time() < self.delay:
+            pass
+        self.proxy.d.close(infohash)
+
+    def erase(self,infohash):
+        while time() < self.delay:
+            pass
+        self.proxy.d.erase(infohash)
+
+    def get_active_infohashes(self):
+        while time() < self.delay:
+            pass
+        return self.proxy.download_list()
