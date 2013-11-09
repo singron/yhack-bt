43a50fee0350363f82baf3158d25a4e21b14efc1
Michael Metrocavich <m.metrocavich@gmail.com>
2013-11-09 10:19:43 -0500

    put back jquery

--- web/torrents.php
+++ web/torrents.php
@@ -2,6 +2,9 @@
 require('controller.class.php'); 
 $user = Controller::authenticate();
 if($user == NULL) header("Location: login.php");
+$user->getActiveJobs();
+date_default_timezone_set('America/New_York'); 
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,13 +28,20 @@ if($user == NULL) header("Location: login.php");
        ?>
        <div class ="upload">
             <form class="form-upload" action="upload.php" method="post" role="form">
-                <label for="torrentfile">Torrent File</label>
+                <div class="btn-group" data-toggle="buttons">
+                    <label class="btn btn-primary">
+                        <input type="radio" name="torrenttype" id="torrent">Torrent
+                    </label>
+                    <label class="btn btn-primary">
+                        <input type="radio" name="torrenttype" id="Magnet">Magnet
+                    </label>
+                </div>
                 <input type="file" id="torrentfile">
-                <button class="btn btn-primary" type="submit" name="uploadtorrentbutton">Upload Torrent</button>
+                <button class="btn btn-default" type="submit" name="uploadtorrentbutton">Upload Torrent</button>
             </form>
         </div
         <div class="torrents">
-          <table class="table">
+          <table class="table" id='torrents'>
             <thead>
               <tr>
                 <th></th>
@@ -54,14 +64,14 @@ if($user == NULL) header("Location: login.php");
                     <?php else: ?>
                         <tr class="active">
                             <td><button type="button" class="btn btn-danger">Cancel</button></td>
-                            <td><?php $job->torrent->bid ?></td>
+                            <td><?php echo $job->bid ?></td>
                     <?php endif; ?>
-                    <td><?php $job->torrent->name ?></td>
-                    <td><?php $job->size ?></td>
+                    <td><?php echo $job->torrent->name ?></td>
+                    <td><?php echo $job->size ?></td>
                     <td>
                         <div class="progress text-center">
-                            <div class="progress-bar" style="width: <?php round(100*($job->downloaded/$job->size),1) ?>%;">
-                                <span><?php round(100*($job->downloaded/$job->size),1) ?>%</span>
+                            <div class="progress-bar" style="width: <?php echo round(100*($job->downloaded/$job->size),1) ?>%;">
+                                <span><?php echo round(100*($job->downloaded/$job->size),1) ?>%</span>
                             </div> 
                         </div>
                     </td>
@@ -70,8 +80,8 @@ if($user == NULL) header("Location: login.php");
                         <td></td>
                         <td></td>
                     <?php else: ?>
-                        <td><?php $job->speed ?></td>
-                        <td><?php $job->eta ?></td>
+                        <td><?php echo $job->speed ?></td>
+                        <td><?php echo $job->eta ?></td>
                         <td></td>
                     <?php endif; ?>
                 <?php endforeach; ?>
@@ -84,5 +94,33 @@ if($user == NULL) header("Location: login.php");
     <script src="https://code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="assets/js/bootstrap.min.js"></script>
+    
+    <script>
+                $(document).ready(function () {
+                    var interval = 3000;  //number of mili seconds between each call
+                    var refresh = function() {
+                                $.ajax({
+                                        type: "POST",
+                                        data: { 'userId' : '<? echo $user->userId; ?>', 'mode' : 'getAvailableJobsForUser' },
+                            url: "controller.php",
+                            cache: false,
+                                        dataType: "json",
+                            success: function(jobs) {
+                                                var torrents = $("#torrents tr");
+                                                for (var job in jobs){
+                                                        $("#torrents tr .progress")[job].style.width = 1.00 * jobs[job]["downloaded"] / jobs[job]["size"];
+                                                        $("#torrents tr .progress span")[job].innerHTML = Math.round(100*(jobs[job]["downloaded"] / jobs[job]["size"]),1) + "%";
+                                                }
+                                setTimeout(function() {
+                                    refresh();
+                                }, interval);
+
+                            }
+                        });
+                    };
+                    refresh();
+                });                
+        </script> 
+
   </body>
 </html>
