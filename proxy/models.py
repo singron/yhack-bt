839fed1707b566846d1646e74621f2a572980542
Eric Culp <eculperic@gmail.com>
2013-11-09 09:23:53 +0000

    Make all model lowercase

--- proxy/models.py
+++ proxy/models.py
@@ -3,8 +3,8 @@ from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, Lar
 metadata = MetaData()
 
 Jobs = Table('jobs', metadata,
-    Column('jobId', Integer, primary_key=True),
-    Column('torrentId', Integer, ForeignKey('Torrents.torrentId')),
+    Column('jobid', Integer, primary_key=True),
+    Column('torrentid', Integer, ForeignKey('torrents.torrentid')),
     Column('added', DateTime(False)),
     Column('bid', Integer),
     Column('downloaded', Integer),
@@ -13,26 +13,26 @@ Jobs = Table('jobs', metadata,
     Column('eta', DateTime(False)),
     Column('completed', DateTime(False)),
     Column('active', Boolean),
-    Column('userId', Integer, ForeignKey('Users.userId')),
-    Column('downloadId', Integer, ForeignKey('Downloads.downloadId')),
+    Column('userid', Integer, ForeignKey('users.userid')),
+    Column('downloadid', Integer, ForeignKey('downloads.downloadid')),
 )
 
 Downloads = Table('downloads', metadata,
-    Column('downloadId', Integer, primary_key=True),
+    Column('downloadid', Integer, primary_key=True),
     Column('start_time', DateTime(False)),
     Column('ip', String, nullable=False),
     Column('link', String, nullable=False),
 )
 
 Torrents = Table('torrents', metadata,
-    Column('torrentId', Integer, primary_key=True),
+    Column('torrentid', Integer, primary_key=True),
     Column('name', String),
     Column('torrent', LargeBinary),
-    Column('infoHash', String, unique=True)
+    Column('infohash', String, unique=True)
 )
 
 Users = Table('users', metadata,
-    Column('userId', Integer, primary_key=True),
+    Column('userid', Integer, primary_key=True),
     Column('email', String),
     Column('hash', String),
     Column('salt', String),
