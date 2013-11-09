91dd51b8ea430e3d5f351840e040b6cb6b6f32c7
Eric Culp <eculperic@gmail.com>
2013-11-09 09:27:00 +0000

    Work on daemon

--- daemon/btdaemon.py
+++ daemon/btdaemon.py
@@ -7,19 +7,19 @@ from sqlalchemy import create_engine,and_,func,select,update,bindparam,not_
 
 def get_info_hash_torrent_file(filedata):
 	info = bencode.bdecode(filedata)['info']
-	infoHash = hashlib.sha1(bencode.bencode(info)).hexdigest()
+	infohash = hashlib.sha1(bencode.bencode(info)).hexdigest()
 
 def get_info_hash_magnet(magnet):
 	parts = urlparse(magnet)
 	xt = parts.query['xt']
-	infoHash = None
+	infohash = None
 	if xt.startswith('urn:btih:'):
-		infoHash = xt[len('urn:btih'):]
+		infohash = xt[len('urn:btih'):]
 	elif xt.startswith('urn:sha1'):
-		infoHash = xt[len('urn:sha1'):]
+		infohash = xt[len('urn:sha1'):]
 	else:
 		error("unknown magnet link hash {}".format(magnet))
-	return infoHash
+	return infohash
 
 def main():
 	queue_size = 50
@@ -28,38 +28,38 @@ def main():
 	complete_qry = Jobs.update()\
 	               .values(completed=func.now())\
 			       .where(and_(Jobs.c.completed == None,\
-				               Jobs.c.torrentId == Torrents.c.torrentId,\
-				               Torrents.c.infoHash == bindparam('infoHash')))
+				               Jobs.c.torrentid == Torrents.c.torrentid,\
+				               Torrents.c.infohash == bindparam('infohash')))
 
 	update_qry = Jobs.update()\
 			     .values(downloaded=bindparam('completed_bytes'),\
 				         speed=bindparam('down_rate'),\
 				         eta=bindparam('eta'),\
 						 size=bindparam('size'))\
-			     .where(and_(Torrents.c.infoHash == bindparam('infoHash'),
-				             Jobs.c.torrentId == Torrents.c.torrentId))
+			     .where(and_(Torrents.c.infohash == bindparam('infohash'),
+				             Jobs.c.torrentid == Torrents.c.torrentid))
 
 	active_queue = rt.get_active_infohashes()
 	running = True
 	while running:
 		active_queue = rt.get_active_infohashes()
 		if active_queue:
-			for infoHash in active_queue:
+			for infohash in active_queue:
 				completed_bytes = rt.get_completed_bytes()
 				size_bytes = rt.get_size_bytes()
 				if completed_bytes == size_bytes:
 					# Torrent is done
-					move_completed_download(rt, infoHash)
-					rt.close(infoHash)
-					engine.execute(complete_qry, infoHash=infoHash)
+					move_completed_download(rt, infohash)
+					rt.close(infohash)
+					engine.execute(complete_qry, infohash=infohash)
 				else:
 					# update current stats
-					down_rate = rt.get_down_rate(infoHash)
-					completed_bytes = rt.get_completed_bytes(infoHash)
-					size = rt.get_size_bytes(infoHash)
+					down_rate = rt.get_down_rate(infohash)
+					completed_bytes = rt.get_completed_bytes(infohash)
+					size = rt.get_size_bytes(infohash)
 					eta = (size - completed_bytes) / down_rate
 					engine.execute(update_qry,\
-							infoHash=infoHash,\
+							infohash=infohash,\
 							down_rate=down_rate,\
 							eta=eta,\
 							completed_bytes=completed_bytes,\
@@ -68,19 +68,19 @@ def main():
 		if not active_queue:
 			# Add more torrents
 			more_needed = queue_size
-			queue_qry = select([Torrents.c.infoHash])\
+			queue_qry = select([Torrents.c.infohash])\
 						.where(and_(Jobs.c.completed == None,\
-									Jobs.c.torrentId == Torrents.c.torrentId))\
+									Jobs.c.torrentid == Torrents.c.torrentid))\
 						.order_by(Jobs.c.bid.desc(), Jobs.c.added.asc())\
 						.limit(more_needed)
 			engine.execute(queue_qry)
 					
 		elif len(active_queue) < queue_size:
 			more_needed = queue_size - len(active_queue)
-			queue_qry = select([Torrents.c.infoHash])\
+			queue_qry = select([Torrents.c.infohash])\
 						.where(and_(Jobs.c.completed == None,\
-									Jobs.c.torrentId == Torrents.c.torrentId,\
-									not_(Torrents.c.infoHash.in_(active_queue))))\
+									Jobs.c.torrentid == Torrents.c.torrentid,\
+									not_(Torrents.c.infohash.in_(active_queue))))\
 						.order_by(Jobs.c.bid.desc(), Jobs.c.added.asc())\
 						.limit(more_needed)
 			engine.execute(queue_qry)
