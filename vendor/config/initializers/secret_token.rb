b0250dbed05e5129b538e21d7e009335756339ce
Michael Napolitano <Moo@Michaels-MacBook-Pro.local>
2013-12-26 22:42:32 -0500

    Let's try that again'

---
+++ vendor/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Btrails::Application.config.secret_key_base = '47f4a149d1b7bb3248d5ff50913031081246b5a4a181da2571eddbfc1c3d6ca8d74186f0a8000193a4681d3a3d729ab25a343ccf68779c2d6d905705e3981843'
